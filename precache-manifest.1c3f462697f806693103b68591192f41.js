def467d373005517afcec8bedf91c9c713006ab4
ekunkoya.seun@gmail.com <ekunkoya.seun@gmail.com>
2019-08-23 07:12:03 +0100

    Updates

---
+++ precache-manifest.1c3f462697f806693103b68591192f41.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6db2996cc1f207a78a55ce20bd12c672",
+    "url": "/seunekunkoya/Expense-Tracker/index.html"
+  },
+  {
+    "revision": "752fcf0fd3dcb54428f7",
+    "url": "/seunekunkoya/Expense-Tracker/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "1dde725335c35fd78af4",
+    "url": "/seunekunkoya/Expense-Tracker/static/css/main.553f390c.chunk.css"
+  },
+  {
+    "revision": "752fcf0fd3dcb54428f7",
+    "url": "/seunekunkoya/Expense-Tracker/static/js/2.5144db86.chunk.js"
+  },
+  {
+    "revision": "1dde725335c35fd78af4",
+    "url": "/seunekunkoya/Expense-Tracker/static/js/main.607a59a8.chunk.js"
+  },
+  {
+    "revision": "0f57dcf2160e5710501e",
+    "url": "/seunekunkoya/Expense-Tracker/static/js/runtime~main.5001ef09.js"
+  },
+  {
+    "revision": "a33c947d2acb0bc67b1af71379a3b72c",
+    "url": "/seunekunkoya/Expense-Tracker/static/media/fundall.a33c947d.svg"
+  },
+  {
+    "revision": "4c759b1f4ee7228f45f5954910d66392",
+    "url": "/seunekunkoya/Expense-Tracker/static/media/fundallDashboard.4c759b1f.svg"
+  },
+  {
+    "revision": "a242c9bc0f9f3b408823f96e9a2af285",
+    "url": "/seunekunkoya/Expense-Tracker/static/media/fundallLogin.a242c9bc.svg"
+  },
+  {
+    "revision": "40c7b9f38c353a6c59e5d2bffe583d43",
+    "url": "/seunekunkoya/Expense-Tracker/static/media/fundalllogo.40c7b9f3.svg"
+  }
+]);
